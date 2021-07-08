bb7054291084b78a6579d7d18934ed9826af16a0
Fred Schumacher <fred.schumacher@colorado.edu>
2021-07-08 11:51:54 -0600

    Add journal and book views

--- sql/counter-r5.sql
+++ sql/counter-r5.sql
@@ -87,3 +87,67 @@ CREATE VIEW title_report_view AS (
         m.period ASC,
         t.title ASC
 );
+
+CREATE VIEW title_report_journal_view AS (
+    SELECT
+        (t.id + m.id) AS id,
+        t.title AS title,
+        t.title_type AS title_type,
+        p.preferred_name AS platform,
+        t.publisher AS publisher,
+        t.doi AS doi,
+        t.proprietary_id AS proprietary_id,
+        t.print_issn AS print_issn,
+        t.online_issn AS online_issn,
+        t.uri AS uri,
+        t.status AS status,
+        m.access_type AS access_type,
+        m.metric_type AS metric_type,
+        m.period as period,
+        m.period_total AS period_total
+    FROM
+        title_report t
+    JOIN
+        metric m ON m.title_report_id = t.id
+    JOIN
+        platform_ref p ON p.id = t.platform_id
+    WHERE
+        title_type = 'J'
+    ORDER BY
+        m.period_total DESC,
+        m.period ASC,
+        t.title ASC
+);
+
+CREATE VIEW title_report_book_view AS (
+    SELECT
+        (t.id + m.id) AS id,
+        t.title AS title,
+        t.title_type AS title_type,
+        p.preferred_name AS platform,
+        t.publisher AS publisher,
+        t.doi AS doi,
+        t.proprietary_id AS proprietary_id,
+        t.isbn AS isbn,
+        t.print_issn AS print_issn,
+        t.online_issn AS online_issn,
+        t.uri AS uri,
+        t.yop AS yop,
+        t.status AS status,
+        m.access_type AS access_type,
+        m.metric_type AS metric_type,
+        m.period as period,
+        m.period_total AS period_total
+    FROM
+        title_report t
+    JOIN
+        metric m ON m.title_report_id = t.id
+    JOIN
+        platform_ref p ON p.id = t.platform_id
+    WHERE
+        title_type = 'B'
+    ORDER BY
+        m.period_total DESC,
+        m.period ASC,
+        t.title ASC
+);
