fe5c5f72605a2f11a1c5bb649f527b65bd1773eb
Fred Schumacher <fred.schumacher@colorado.edu>
2021-07-16 12:29:47 -0600

    Modify JR1 report class

--- dataloader/jr1report.py
+++ dataloader/jr1report.py
@@ -12,54 +12,56 @@ class JR1Report:
     anomalies may arise and/or code using this class may raise an exception.
     """
 
+    PERIODS = ['', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug',
+        'sep', 'oct', 'nov', 'dec']
+    MAX_ROWS = 1048576
+    MAX_COLS = 16384
+    HEADER_ROW = 8
+    DATA_ROW_START = 10
+    DATA_COL_START = 1
+    DATA_COL_END = 22
+
     def __init__(self, workbook):
         self._workbook = openpyxl.load_workbook(filename=workbook, data_only=True)
         self._worksheet = self._workbook.active
-        self._offset = self._get_offset()
-        self._period = self._worksheet.cell(row=5+self._offset, column=1).value
+        self._reporting_period = self._worksheet.cell(row=5, column=1).value
         self._filename = os.path.basename(workbook)
     
     @property
     def filename(self):
-        """
-        Returns the Excel file name
-        """
-        
         return self._filename
     
     @property
-    def platform(self):
-        """
-        Returns the platform name represented in cell C10
-        """
-        
-        return self._worksheet.cell(row=10+self._offset, column=3).value
-
-    @property
-    def period(self):
-        """
-        Returns the report period represented in cell A5
-        """
-        
-        return self._period
-    
-    @property
     def period_from(self):
-        """
-        Returns the period from date represented in cell A5
-        """
-        
-        return datetime.strptime(self._period.strip()[:10], '%Y-%m-%d')
+        return self._reporting_period.split('to')[0].strip()
 
     @property
     def period_to(self):
-        """
-        Returns the period to date represented in cell A5
-        """
-        
-        return datetime.strptime(self._period.strip()[-10:], '%Y-%m-%d')
+        return self._reporting_period.split('to')[1].strip()
 
-    def data_range(self):
+    def _header_row(self):
+        """
+        Returns the header row
+
+        The header row for J1 (R4) report consists of 10 static
+        columns and a variable number of date columns, depending
+        on the reporting period. For a 12 month report, there will
+        be 12 date columns (Jan - Dec).
+        """
+
+        # Start with non-date columns, which are static, and then
+        # append the date column headings
+        header = ['title', 'publisher', 'platform', 'doi',
+            'proprietary_id', 'print_issn', 'online_issn']
+        start_month = int(self.period_from.split('-')[1])
+        end_month = int(self.period_to.split('-')[1])
+        period_cols = self.PERIODS[start_month:end_month+1]
+        for month in period_cols:
+            header.append(month)
+
+        return header
+
+    def data_rows(self):
         """
         Returns the range of rows containing publication data.
 
@@ -67,52 +69,46 @@ class JR1Report:
         starts at row 10 in the spreadsheet.
         """
 
-        row_num = 10 + self._offset
-        for row in self._worksheet.iter_rows(min_row=10+self._offset, min_col=1, max_row=1048576, max_col=1):
-            if row[0].value == None:
+        r = self.DATA_ROW_START
+        for row in self._worksheet.iter_rows(min_row=self.DATA_ROW_START, min_col=1,
+            max_row=self.MAX_ROWS, max_col=1, values_only=True):
+            if row[0] is None:
                 break
-            row_num = row_num + 1
+            r += 1
             
-        return list(range(10+self._offset, row_num))
+        return list(range(self.DATA_ROW_START, r))
     
-    def get_row(self, row_num):
+    def data_cols(self):
+        """
+        Returns the range of data columns. The actual number of columns
+        depends on the reporting period.
+        """
+        c = 1
+        for col in self._worksheet.iter_cols(min_row=self.HEADER_ROW,
+            min_col=self.DATA_COL_START, max_row=self.HEADER_ROW,
+            max_col=self.MAX_COLS, values_only=True):
+            if col[0] is None:
+                break
+            c += 1
+
+        return list(range(self.DATA_COL_START,c))
+
+    def get_row(self, n):
         """
         Returns a complete row of publication data for the given row number.
 
         For a JR1 report covering twelve months of usage data (typically Jan-Dec),
-        there will be 22 columns of data in the row. The first item in the list
-        will always be the given row number. Reports containing less (or more)
+        there will be 22 columns of data in the row. Reports containing less (or more)
         months of usage data will have less or more list items, respectively.
         """
 
         datarow = list()
-        datarow.append(row_num)
-        for row in self._worksheet.iter_cols(min_row=row_num, min_col=1, max_row=row_num, max_col=22):
+        for row in self._worksheet.iter_cols(min_row=n, min_col=1,
+            max_row=n, max_col=self.DATA_COL_END):
             for cell in row:
-                if cell.value != None: # Check for empty cells
-                    datarow.append(str(cell.value).replace('"', ''))
-                else:
+                if cell.value is None: # Check for empty cells
                     datarow.append('')
+                else:
+                    datarow.append(str(cell.value).replace('"', '').strip())
         
         return datarow
-
-    def _get_offset(self):
-        """
-        Returns the offset (if any) if the starting row is not 1.
-        
-        The COUNTER 4 standard requires that the JR1 report start at row 1 with
-        the text 'Journal Report 1(R4)' in the first cell (A1). If the report begins
-        on a subsequent row, the value of A1 will be different.
-        """
-        
-        offset = 0
-        if not str(self._worksheet.cell(row=1, column=1).value).startswith('Journal Report'):
-            offset = 1
-            while True:
-                contents = str(self._worksheet.cell(row=1+offset, column=1).value)
-                if contents.startswith('Journal Report'):
-                    break
-                else:
-                    offset += 1
-        
-        return offset
